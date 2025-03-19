b838971d84218cc2138a818909c002dc36167586
devsantos1999 <devsantos1999@gmail.com>
2025-03-19 16:49:31 -0300

    learning react
    - props
    - multiple rendering of objects
    - destructuring
    - states pt 2

--- React/academy/src/App.jsx
+++ React/academy/src/App.jsx
@@ -4,19 +4,44 @@ import "./App.css"
 import { useState } from "react";
 
 function App() {
-  const [tasks] = useState([
+  const [tasks, setTasks] = useState([
     {
       id: 1,
-      title: "Sla"
+      title: "Fazer um filho",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: "Ter um irmão",
+      isCompleted: false
     }
   ])
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map(task => {
+      
+      if(task.id == taskId){
+        task.isCompleted = !task.isCompleted
+        return task
+      }
+
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
+  function onTaskRemove(taskId) {
+    const newTask = tasks.filter(task => task.id != taskId)
+    setTasks(newTask)
+  }
+
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px]">
-        <h1 className="font-bold text-3xl text-center">Gerenciador de tarefas</h1>
+    <div className="w-screen h-screen bg-gray-900 flex justify-center p-6">
+      <div className="w-[600px] space-y-6">
+        <h1 className="font-bold text-3xl text-center text-white">Gerenciador de tarefas</h1>
         <AddTask />
-        <Tasks tasks={tasks} />
+          <Tasks tasks={tasks} onTaskClick = {onTaskClick} onTaskRemove = {onTaskRemove}/>
       </div>
     </div>
     
--- React/academy/src/components/Tasks.jsx
+++ React/academy/src/components/Tasks.jsx
@@ -1,6 +1,23 @@
-function Tasks(props) {
-    console.log(props)
-    return <h1>{props.tasks[0].title}</h1>
+import { ChevronRightIcon, DeleteIcon } from "lucide-react";
+
+function Tasks({tasks, onTaskClick, onTaskRemove}) {
+  return (
+    <ul className="space-y-2 bg-gray-800 p-6 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key= {task.id} className="flex space-x-4">
+          <button onClick={() => onTaskClick(task.id)} className={`flex bg-gray-950 w-full text-left text-white p-2 rounded-md ${task.isCompleted ? 'line-through' : ''}`} >
+            {task.title}
+          </button>
+          <button className="bg-gray-950  text-white p-2 rounded-md">
+            <ChevronRightIcon />
+          </button>
+          <button onClick={() => onTaskRemove(task.id)} className="bg-gray-950  text-white p-2 rounded-md">
+            <DeleteIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks
