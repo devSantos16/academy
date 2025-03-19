2d533f1defe9bb4312f70d339d4cf671080f0abc
devsantos1999 <devsantos1999@gmail.com>
2025-03-19 15:09:43 -0300

    learning react and tailwind
    - state

--- React/academy/src/App.jsx
+++ React/academy/src/App.jsx
@@ -1,11 +1,27 @@
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import "./App.css"
+import { useState } from "react";
+
 function App() {
-  const message = "Hello world"
+  const [tasks] = useState([
+    {
+      id: 1,
+      title: "Sla"
+    }
+  ])
+
   return (
-    <div>
-      <h1>{message}</h1>
-      <h1>{message}</h1>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="font-bold text-3xl text-center">Gerenciador de tarefas</h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
+    
   );
 }
 
-export default App;
+
+export default App
